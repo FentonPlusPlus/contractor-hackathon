4b2b0ce0aabd9a04aab7d22b32d5a81167a7c494
Julio Camargo <jcamargo@hotmail.co.uk>
2023-03-30 13:52:15 +0100

    forms

---
+++ my-app/pages/add-contract.js
@@ -0,0 +1,33 @@
+import Navbar from '../components/Navbar/navbar';
+
+
+export default function AddContract(){
+  return(
+    <div>
+    <Navbar />
+    <h1> Add a Contract</h1>
+    <form>
+      <label>
+        Company:
+        <input type="text" name="Company" />
+      </label>
+      <label>
+        Job Title:
+        <input type='text' name='Job Title' />
+      </label>
+      <label>
+        Start Date:
+        <input type='text' name='Start Date' />
+      </label>
+      <label>
+        End Date:
+        <input type='text' name='End Date' />
+      </label>
+      <label>
+        Contact:
+        <input type='text' name='Contact' />
+      </label>
+    </form>
+    </div>
+    
+  )}
